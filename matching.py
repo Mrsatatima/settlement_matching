caa4a2f8d23b27aa9035ede0fa05ac46af5f9a99
Al-amin Ahmad Abbas <aminuozil@gmail.com>
2023-04-12 00:01:12 +0100

    Add get_captured_list function

--- matching.py
+++ matching.py
@@ -66,3 +66,51 @@ def get_p3b_list(df, LGA, p3b=True):
 
     # Return the p3b_list dictionary and the settlement count
     return p3b_list, count
+
+def get_captured_list(df, LGA, grid3=False):
+
+    """
+        Returns a dictionary of captured settlements in a given Local Government Area
+        (LGA) with their corresponding coordinates.
+
+        Args:
+        df (pandas DataFrame): The data containing settlement, LGA, ward, latitude, 
+                                longitude, and, if grid3 is False, altitude and accuracy.
+        LGA (str): The name of the Local Government Area.
+        grid3 (bool, optional): A boolean indicating whether to include altitude and accuracy 
+                                values in the output. Defaults to False. It salso indicates 
+                                whether the file is RR_colect or grid3 dataset
+
+        Returns:
+        dict: A dictionary where keys are LGAs and values are dictionaries where keys are wards and values are dictionaries
+             where keys are settlements and values are strings of latitude and longitude separated by a '|' character, or, if grid3 is True, separated by '|' and followed by altitude and accuracy separated by '|'.
+
+    """
+    captured_list = {}
+
+    # Iterate over each row in the dataframe
+    for idx in range(len(df)):
+        # Get the name of the settlement, LGA, ward, latitude, and longitude
+        settlement = " ".join(str(df["Name of Settlement"][idx]).lower().replace(".","").replace(".","").replace(")","").replace("(","").strip().split())
+        lga = str(df["LGA"][idx]).lower().strip()
+        ward = str(df["Ward"][idx]).lower().strip()
+        latitude= str(df["Latitude"][idx]).lower().strip()
+        longitude = str(df["Longitude"][idx]).lower().strip()
+
+        # If grid3 is False, also get the accuracy and altitude values
+        if not grid3:
+            accuracy = str(df["Acurracy"][idx]).lower().strip()
+            altitude = str(df["Altitude"][idx]).lower().strip()
+
+        # If the LGA matches the requested LGA
+        if lga == LGA.lower():
+            # Add the settlement to the captured_list dictionary
+            if lga not in captured_list:
+                captured_list[lga] = {}
+            if ward not in captured_list[lga]:
+                captured_list[lga][ward] = {}
+            if settlement not in captured_list[lga][ward]:
+                # If grid3 is False, only add latitude and longitude to the captured_list value
+                captured_list[lga][ward][settlement] = f"{latitude}|{longitude}" if not grid3 else f"{latitude}|{longitude}|{accuracy}|{altitude}"
+
+    return captured_list
